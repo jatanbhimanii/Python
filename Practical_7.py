39cdc4f90f2c02c605dcbe04406e2f6b3da69004
jatanbhimanii <64524798+jatanbhimanii@users.noreply.github.com>
2022-02-19 12:32:36 +0530

    Add files via upload

---
+++ Practical_7.py
@@ -0,0 +1,15 @@
+n = int(input())
+while n:
+    n -= 1
+    string = input()        # input is already in string format so no need to type-cast
+    halve = len(string) // 2
+    if len(string) % 2 == 0:
+        if sorted(string[:halve]) == sorted(string[halve:]):
+            print('YES')
+        else:
+            print('NO')
+    else:
+        if sorted(string[:halve]) == sorted(string[halve + 1:]):
+            print('YES')
+        else:
+            print('NO')
